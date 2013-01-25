a254b5ca013776de20864ce2c0519f5b0c236bbb
shimizukawa <takayuki.shimizukawa@beproud.jp>
2013-01-25 11:45:50 +0900

    use attribute-override-feature for code shorten

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,9 +18,20 @@
 #
 
 default[:chefenv][:user]            = 'root'
-default[:chefenv][:dotchefpath]     = '/root/.chef'
+default[:chefenv][:dotchefpath]     = nil
 default[:chefenv][:node_name]       = ENV['USER']
 default[:chefenv][:chef_server_url] = 'http://localhost:4000'
 default[:chefenv][:copyright]       = 'cookbook'
 default[:chefenv][:email]           = 'cookbook@example.com'
 default[:chefenv][:license]         = 'apachev2'
+
+# setup dynamic attribute
+
+unless node[:chefenv][:dotchefpath]
+  if node[:chefenv][:user] == 'root'
+    path = '/root/.chef'
+  else
+    path = "/home/#{node[:chefenv][:user]}/.chef"
+  end
+  override[:chefenv][:dotchefpath] = path
+end
