98137489443ae3bd43c414611dedbdc9ceddb14c
shimizukawa <takayuki.shimizukawa@beproud.jp>
2013-07-17 16:50:24 +0900

    change quote

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'chefenv'
+name             "chefenv"
 maintainer       'Takayuki SHIMIZUKAWA'
 maintainer_email 'shimizukawa@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures chefenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'git'
